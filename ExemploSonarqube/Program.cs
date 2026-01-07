51924df1b01a4a6e98e3c38d7a94b67b884baf23
Ana Esteves <11687547+profana@users.noreply.github.com>
2026-01-07 13:10:34 +0000

    Add project files.

---
+++ ExemploSonarqube/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace SchoolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sistema de Gestão de Alunos");
+
+            List<int> notas = new List<int>();
+            notas.Add(10);
+            notas.Add(15);
+            notas.Add(20);
+            notas.Add(-5); // valor inválido
+
+            CalcularMedia(notas);
+
+            if (notas.Count > 0)
+            {
+                Console.WriteLine("Programa terminado.");
+            }
+        }
+
+        static void CalcularMedia(List<int> notas)
+        {
+            int soma = 0;
+
+            for (int i = 0; i < notas.Count; i++)
+            {
+                soma = soma + notas[i];
+            }
+
+            int media = soma / notas.Count;
+
+            if (media >= 10)
+            {
+                Console.WriteLine("Aluno aprovado com média: " + media);
+            }
+            else
+            {
+                Console.WriteLine("Aluno reprovado com média: " + media);
+            }
+
+            if (media >= 18)
+            {
+                Console.WriteLine("Excelente aluno");
+            }
+
+            if (media >= 18)
+            {
+                Console.WriteLine("Excelente aluno");
+            }
+        }
+    }
+}
